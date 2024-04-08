1070bdb8f5f4f8fa14bd4a7c620fa3b82d96684e
nileshMaurya123 <108219221+nileshMaurya123@users.noreply.github.com>
2024-04-08 06:59:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version of Javascript
+
+// alert(3+3) // we are using node js not browser thats why error is coming
+
+console.log(3+3); console.log('Hitesh')
+
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null // empty value undefined state
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol // for uniqueness symbols are used
+
+
+
+// object
+
+console.log(typeof hitesh);
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
