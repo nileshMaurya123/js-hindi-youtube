fc37548860a98a5d0343e1916917cdf85ae9c317
nileshMaurya123 <108219221+nileshMaurya123@users.noreply.github.com>
2024-05-21 13:12:40 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -13,9 +13,26 @@ const id = Symbol(123)
 const anotherId=Symbol('123')
 console.log(id === anotherId);
 
-const bigNumber = 34567568734567n
+//const bigNumber = 34567568734567n
 
 // Reference(Non primitive)
 
 // Array, Objects, Functions
 
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22
+};
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+//console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof heros);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
