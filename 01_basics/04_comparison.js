ff072f90d979401d24fa2fb0adbd29933ebc308c
nileshMaurya123 <108219221+nileshMaurya123@users.noreply.github.com>
2024-05-21 08:51:15 +0000

    changes done

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -13,3 +13,12 @@ console.log(null>0);
 console.log(null==0);
 console.log(null>=0);
 
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined>0);
+
+
+// ===
+
+console.log("2"===2);
+
