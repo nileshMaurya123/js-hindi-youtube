dd159660eccd6ff48fe15c07672c80a9dbed1db6
nileshMaurya123 <108219221+nileshMaurya123@users.noreply.github.com>
2025-05-08 14:33:06 +0000

    changes done

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,18 @@
+// Dates
+
+   let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+//let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("2023-1-14")
+// console.log(myCreatedDate.toLocaleString());
+
+ let myCreatedDate = new Date("01-14-2023")
+ console.log(myCreatedDate.toLocaleString());
