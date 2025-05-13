aa1742604d58d6732a2543159ed06a8522649c98
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-13 15:16:26 +0530

    changes done

--- 01_basics/07_dateinjs.js
+++ 01_basics/07_dateinjs.js
@@ -18,7 +18,7 @@ let myCreatedDate = new Date("01-14-2023");
 console.log(myCreatedDate.toLocaleString());
 
 // abcd
-console.log(abcd);
+//console.log(abcd);
 
 let newDate = new Date();
 console.log(newDate);
