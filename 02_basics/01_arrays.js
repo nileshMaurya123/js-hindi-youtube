071c6a8f0c475e09dcc16f616a06a5c4fb2c830f
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-13 15:54:39 +0530

    changes done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,4 +4,9 @@ const myArr = [0, 1, 2, 3, 4, 5];
 const myHeors = ["shaktiman", "naagraaj"];
 
 const myArr2 = new Array(1, 2, 3, 4);
-console.log(myArr[0]);
+console.log(myArr[1]);
+
+// Array methods
+
+myArr.push(6);
+console.log(myArr);
