1df41d6bc19b81f259b1168feb6c12fd35dc4842
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-13 16:50:10 +0530

    changes done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,17 +1,24 @@
-// array
-
+// Array declarations
 const myArr = [0, 1, 2, 3, 4, 5];
-const myHeors = ["shaktiman", "naagraaj"];
-
+const myHeros = ["shaktiman", "naagraaj"];
 const myArr2 = new Array(1, 2, 3, 4);
-console.log(myArr[1]);
+
+console.log(myArr[1]); // Output: 1
 
 // Array methods
+myArr.push(6); // Adds 6 to the end
+myArr.push(7); // Adds 7 to the end
+myArr.pop(); // Removes last item (7)
 
-//myArr.push(6);
-//myArr.push(7);
-//myArr.pop()
+myArr.unshift(10); // Adds 10 to the start
+myArr.shift(); // Removes the first item (10)
 
-myArr.unshift(9);
+console.log(myArr.includes(9)); // false
+console.log(myArr.indexOf(3)); // 3
+
+// Use a new variable name to avoid redeclaration error
+const myArrStr = ["0", "1", "2", "3", "4", "5"];
+const newArr = myArrStr.join(); // Joins with commas
 
 console.log(myArr);
+console.log(newArr); // Output: "0,1,2,3,4,5"
