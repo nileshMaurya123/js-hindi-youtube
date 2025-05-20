2829dd5ff8be15ae283ac0c0b61a60bb8daabb12
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-20 21:45:01 +0530

    changes done

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -55,3 +55,10 @@ const regularUser = {
 };
 
 //console.log(regularUser.fullname.userfullname.firstname.lastname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+// const obj3 = {obj1,obj2}
+const obj3 = Object.assign({}, obj1, obj2);
+console.log(obj3);
