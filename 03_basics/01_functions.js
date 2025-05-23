271b336dba5c524b52c498a46b5487c771dbb668
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-23 08:44:23 +0530

    changes done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,3 +2,13 @@ function addTwoNumbers(number1, number2) {
   console.log(number1 + number2);
 }
 addTwoNumbers(3, 4);
+
+const result = addTwoNumbers(3, 5);
+
+console.log("Result:", result);
+
+function addTwoNumbers(number1, number2) {
+  let result = number1 + number2;
+  console.log("Hitesh");
+  return result;
+}
