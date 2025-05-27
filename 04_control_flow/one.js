b8352cc3b0405cd93d79daef48177fdf50a713dd
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-27 11:05:43 +0530

    changes done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -5,4 +5,17 @@ if (2 == "2") {
   console.log("executed");
 }
 
-// <, >, <=, >= , ==, !=, ===
+// <, >, <=, >= , ==, !=, === , !==
+
+if (2 != "3") {
+  console.log("not executed");
+}
+
+let balance = 600;
+
+if (balance < 500) {
+  // < less than sign
+  console.log("Unable to withdrawl");
+} else {
+  console.log(" you can withdrawl");
+}
