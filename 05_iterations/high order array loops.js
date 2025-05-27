963b7fcba659221dd1c42b5e7e62eef50ce83525
nileshMaurya123 <nilesh7874@gmail.com>
2025-05-27 13:51:17 +0530

    changes done

--- 05_iterations/high order array loops.js
+++ 05_iterations/high order array loops.js
@@ -66,3 +66,27 @@ function sum(accumulator, currentValue) {
 const total = prices.reduce(sum);
 
 console.log(`$${total.toFixed(2)}`);
+
+// array = a variable like structure that can hold more than 1 value
+
+let fruits = ["apple", "orange", "banana", "coconut"];
+
+//fruits.push("coconut");
+//fruits.pop();
+//fruits.unshift("mango");
+// fruits.shift();
+
+// iterate an array
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+
+let numOfFruits = fruits.length;
+let index = fruits.indexOf("banana");
+
+console.log(numOfFruits);
