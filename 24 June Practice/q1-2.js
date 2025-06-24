79ac5e028c31527d5295a1832a8b7dd5956235b6
nileshMaurya123 <nilesh7874@gmail.com>
2025-06-24 13:30:20 +0530

    changes done

---
+++ 24 June Practice/q1-2.js
@@ -0,0 +1,16 @@
+// ### 1. **Create an array**
+
+// > Create an array named `fruits` with the values `"apple"`, `"banana"`, and `"orange"`.
+
+// ---
+
+// ### 2. **Access an element**
+
+// > Access the second item in the `fruits` array and store it in a variable called `secondFruit`.
+
+function accessItem() {
+  let fruits = ["apple", "banana", "orange"];
+  console.log(fruits[1]);
+}
+
+accessItem();
---
+++ 24 June Practice/q3.js
@@ -0,0 +1,11 @@
+// ### 3. **Add an element to the end**
+
+// > Add `"grape"` to the end of the `fruits` array using the appropriate array method.
+
+function addElement() {
+  let fruits = ["apple", "banana"];
+  fruits.push("grape"); // use push, and "grape" should be a string in quotes
+  console.log(fruits);
+}
+
+addElement();
